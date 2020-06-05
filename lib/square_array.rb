4a0c0487f66064da84353942d18c82897982e774
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-06-05 15:55:08 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  # your code
+  i = 0 
+  new_array = []
+  while i < array.length do
+    new_array << array[i] ** 2
+    i += 1
+  end
+  p new_array
 end
